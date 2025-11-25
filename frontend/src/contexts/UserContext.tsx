3e74088afe949d926c80ee8bbc314a530e8bc38b
lindsey5 <lindseysamson5@gmail.com>
2025-11-25 23:59:54 +0800

    commit

--- frontend/src/contexts/UserContext.tsx
+++ frontend/src/contexts/UserContext.tsx
@@ -20,7 +20,7 @@ export const UserContextProvider = <T,>({ children }: UserContextProviderProps<T
   const { data, loading } = useFetch('/api/auth');
 
   return (
-    <UserContext.Provider value={{ user: { ...data.user, role: data.role } as T | null, loading }}>
+    <UserContext.Provider value={{ user: { ...data?.user, role: data?.role } as T | null, loading }}>
       {children}
     </UserContext.Provider>
   );
