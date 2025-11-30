83ec9711e8d668e04e33d0fc503191a947f8769a
lindsey5 <lindseysamson5@gmail.com>
2025-11-30 13:19:58 +0800

    change the incomes chart title

--- frontend/src/components/ui/MonthlyIncomes.tsx
+++ frontend/src/components/ui/MonthlyIncomes.tsx
@@ -9,7 +9,7 @@ const MonthlyIncomes = () => {
         <div className="h-[500px] bg-white border border-gray-200 shadow-lg w-full relative flex justify-center">
             {loading ? <CircularProgress /> : (
                 <LineChart 
-                    title="Month Incomes"
+                    title={`Monthly Incomes ${new Date().getFullYear()}`}
                     data={data?.incomes.map((income : any) => ({ label: income.month, value: income.total }))}
                     yName="Income"
                 />
